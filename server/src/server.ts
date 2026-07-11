0c49aa7c33d9c1322af3be6186930f9bc84ed6fe
Anugrah <your-email@example.com>
2026-07-11 15:52:33 +0530

    fix(server): change db name

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
-import connectDB from "./config/config.db.js";
+import connectDB from "./config/db.config.js";
 
 const PORT = process.env.PORT || 5000;
 const startServer = async () => {
