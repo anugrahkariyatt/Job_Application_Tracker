e7e1686414974d0132c8831fff706d1b5c7b3981
Anugrah <your-email@example.com>
2026-07-12 19:45:46 +0530

    feat(candidate): implement candidate management features including creation, updates, profile image and resume handling

---
+++ server/src/services/candidate.service.ts
@@ -0,0 +1,92 @@
+import Candidate from "../models/candidate.model.js";
+import { AppError } from "../utils/AppError.js";
+import {
+  CreateCandidateInput,
+  UpdateCandidateInput,
+} from "../validations/candidate.validations.js";
+import { uploadImage } from "./cloudinary.service.js";
+
+export const createCandidate = async (
+  userId: string,
+  data: CreateCandidateInput,
+) => {
+  const existingCandidate = await Candidate.findOne({
+    userId,
+  });
+
+  if (existingCandidate) {
+    throw new AppError("Candidate profile already exists", 400);
+  }
+
+  const candidate = await Candidate.create({
+    userId,
+    ...data,
+  });
+
+  return candidate;
+};
+
+export const getMyCandidate = async (userId: string) => {
+  const candidate = await Candidate.findOne({ userId });
+
+  if (!candidate) {
+    throw new AppError("Candidate profile not found", 404);
+  }
+
+  return candidate;
+};
+
+export const updateCandidate = async (
+  userId: string,
+  data: UpdateCandidateInput,
+) => {
+  const candidate = await Candidate.findOne({ userId });
+
+  if (!candidate) {
+    throw new AppError("Candidate profile not found", 404);
+  }
+
+  Object.assign(candidate, data);
+
+  await candidate.save();
+
+  return candidate;
+};
+
+export const updateCandidateProfileImage = async (
+  userId: string,
+  file: Express.Multer.File,
+) => {
+  const candidate = await Candidate.findOne({ userId });
+
+  if (!candidate) {
+    throw new AppError("Candidate profile not found", 404);
+  }
+
+  const uploadedImage = await uploadImage(file, "candidate/profile-image");
+
+  candidate.profileImage = uploadedImage.secure_url;
+
+  await candidate.save();
+
+  return candidate;
+};
+
+export const updateCandidateResume = async (
+  userId: string,
+  file: Express.Multer.File,
+) => {
+  const candidate = await Candidate.findOne({ userId });
+
+  if (!candidate) {
+    throw new AppError("Candidate profile not found", 404);
+  }
+
+  const uploadedResume = await uploadImage(file, "candidate/resume");
+
+  candidate.resumeUrl = uploadedResume.secure_url;
+
+  await candidate.save();
+
+  return candidate;
+};
